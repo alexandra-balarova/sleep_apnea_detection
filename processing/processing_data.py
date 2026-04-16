6801bbc7b4aaa147dc9035378b1589dffd362955
alexandra-balarova <24IMC10507@fh-krems.ac.at>
2026-04-16 16:53:34 +0200

    rrpeak feature extraction per minute

--- processing/processing_data.py
+++ processing/processing_data.py
@@ -1,10 +1,13 @@
 import datetime
 import os                          # For file and directory handling
+import time
 import numpy as np                # Numerical operations
-import matplotlib.pyplot as plt   # Plotting
+from numpy.ma.core import sqrt
 from scipy.signal import butter, filtfilt  # Signal filtering
 import wfdb                       # Reading .dat + .hea ECG files
 from biosppy.signals import ecg   # QRS detection (R-peak detection)
+import pandas
+from multiprocessing import Pool, cpu_count
 
 def preprocess_signal(signal, sampling_rate):
     """
@@ -35,110 +38,64 @@ def preprocess_signal(signal, sampling_rate):
 
     b, a = butter(4, [low_cutoff, high_cutoff], btype='band', fs=sampling_rate)
     filtered_signal = filtfilt(b, a, signal, axis=-1)
-    print("Filter successful ", b, a)
-    print("Filtered signal: ", filtered_signal)
     #works
     return filtered_signal
 
 def downsampling(filtered_signal, sampling_rate):
     downsample_factor = 2
     downsampled_signal = filtered_signal[:, ::downsample_factor]
-    print("Downsampling success: ", downsampled_signal)
     # works
     new_sampling_rate = sampling_rate / downsample_factor
-    # problem
-    print("New sampling rate: ", new_sampling_rate)
 
     return downsampled_signal, new_sampling_rate
 
-#plotting
-def plot_ecg(signal, sampling_rate, title="ECG Signal"):
-    """
-    Plots a single ECG lead.
-
-    Parameters:
-        signal (np.array): 1D ECG signal
-        sampling_rate (float): Sampling frequency in Hz
-        title (str): Plot title
-    """
-
-    # Create time axis (seconds)
-    time = np.arange(0, len(signal)) / sampling_rate
-
-    # Plot signal
-    plt.figure(figsize=(12, 6))
-    plt.plot(time, signal)
-    plt.title(title)
-    plt.xlabel("Time (seconds)")
-    plt.ylabel("Amplitude")
-    plt.grid(True)
-    plt.show()
-
-#R peak detection
+def resample_to_seconds(timestamps, values, duration):
+    time_grid = np.arange(0, int(duration))
+    resampled = np.interp(time_grid, timestamps, values)
+    return time_grid, resampled
 
 def detect_qrs_complex(signal, sampling_rate):
-    """
-    Detects R-peaks (QRS complexes) in ECG signal.
-
-    Parameters:
-        signal (np.array): 1D ECG signal
-        sampling_rate (float): Sampling frequency
-
-    Returns:
-        qrs_indices (np.array): Indices of detected R-peaks
-    """
-
     # biosppy ECG processing returns a dictionary
     # 'rpeaks' contains indices of detected QRS complexes
-    print("im in qrs")
-
-    #PROBLEM OCCURS HERE
     output = ecg.ecg(
         signal=signal,
         sampling_rate=sampling_rate,
-        show=False
+        show = False
     )
-    print("output of ecg.ecg: ", output)
-    qrs_indices = output['rpeaks']
-    print("qrs indices: ", qrs_indices)
-    return qrs_indices
 
+    return output['rpeaks'], output['heart_rate'], output['heart_rate_ts']
 
 #rr intervals
 
 def calculate_rr_intervals(qrs_indices, sampling_rate):
     """
-    Computes RR intervals (time between heartbeats).
-
-    Parameters:
-        qrs_indices (np.array): R-peak indices
-        sampling_rate (float): Sampling frequency
-
-    Returns:
         rr_intervals (np.array): RR intervals in seconds
     """
-
     # Difference between consecutive R-peaks
     rr_intervals = np.diff(qrs_indices) / sampling_rate
 
     return rr_intervals
 
+def get_features(rr_intervals):
+    rr_mean = np.mean(rr_intervals)
+    rr_sd = np.std(rr_intervals)
+    differences = []
+    for i in range(len(rr_intervals)-1):
+        current = rr_intervals[i]
+        next = rr_intervals[i+1]
+        differences.append((next - current)**2)
+    summed = sum(differences)
+    rmssd = sqrt((1 / (len(rr_intervals) - 1)) * summed)
+    nn50 = 0
+    for rr in rr_intervals:
+        if rr > 0.50:
+            nn50+=1
+    pnn50 = nn50/len(rr_intervals)
+
+    return rr_mean, rr_sd, rmssd, nn50, pnn50
+
 #load data
 def load_dat_ecg(file_path):
-    """
-    Loads ECG signal from WFDB .dat file.
-
-    Important:
-        Requires both .dat and .hea files.
-
-    Parameters:
-        file_path (str): Path to .dat file
-
-    Returns:
-        signal (np.array): ECG signal (channels x samples)
-        sampling_rate (int): Sampling frequency
-    """
-
     # Remove .dat extension (wfdb expects base filename)
     record_name = file_path.replace('.dat', '')
 
@@ -152,16 +109,6 @@ def load_dat_ecg(file_path):
     # Sampling frequency
     sampling_rate = record.fs
 
-    print(f"Sampling rate: {sampling_rate} Hz")
-    print(f"Signal shape: {signal.shape}")
-    print(f"Data type: {signal.dtype}")
-    print(f"Contains NaN: {np.isnan(signal).any()}")
-    print(f"Contains Inf: {np.isinf(signal).any()}")
-    print(f"Min value: {np.min(signal):.6f}")
-    print(f"Max value: {np.max(signal):.6f}")
-    print(f"Mean value: {np.mean(signal):.6f}")
-    print(f"Std deviation: {np.std(signal):.6f}")
-
     # Check for unusual values
     if np.max(np.abs(signal)) > 1e6:
         print("WARNING: Extremely large values detected!")
@@ -170,84 +117,81 @@ def load_dat_ecg(file_path):
         print("WARNING: Zero standard deviation - signal is constant!")
 
     # Check first few samples
-    print(f"First 10 samples: {signal[0, :10]}")
-
     return signal, sampling_rate
 
+def process_segment(signal, fs):
+
+    qrs_indices, heart_rate, heart_rate_ts = detect_qrs_complex(signal, fs)
+
+    rr_intervals = calculate_rr_intervals(qrs_indices, fs)
+
+    rr_mean, rr_sd, rmssd, nn50, pnn50 = get_features(rr_intervals)
+
+    return rr_mean, rr_sd, rmssd, nn50, pnn50
+
 # MAIN PROCESSING LOOP
+def process_file(file_path):
+    try:
+        print(f"\nProcessing file: {os.path.basename(file_path)}")
+        signal, fs = load_dat_ecg(file_path)
+        processed_signal = preprocess_signal(signal, fs)
+
+        # select one channel from filtered signal
+        lead_signal = processed_signal[0]
+
+        #get a sample every minute
+        samples_per_min = int(fs * 59.99)  # one minute
+        length_of_signal = len(lead_signal)
+        num_segments = length_of_signal // samples_per_min
+
+        records = []
+
+        for i in range(num_segments):
+            start = i * samples_per_min
+            end = start + samples_per_min
+            segment = lead_signal[start:end]
+
+            rr_mean, rr_sd, rmssd, nn50, pnn50 = process_segment(segment,fs)
+            records.append({ "title": f"{os.path.basename(file_path)}_{i}",
+                            "rr_mean": rr_mean,
+                            "rr_sd":rr_sd,
+                            "rmssd": rmssd,
+                            "nn50": nn50,
+                            "pnn50": pnn50})
+
+        return records
+
+
+    except Exception as e:
+        print(f"Error processing file {os.path.basename(file_path)}: {e}")
 
 def process_all_ecg_files(root_dir):
-    """
-    Iterates through directory and processes all .dat ECG files.
-
-    Parameters:
-        root_dir (str): Root directory containing ECG records
-    """
-    start_time = datetime.datetime.now()
-
+    start_time = time.time()
     # Walk through directory tree
-    for root, dirs, files in os.walk(root_dir):
+    file_paths = []
+    i =0
+    for root, _, files in os.walk(root_dir):
         for file in files:
+            if file.endswith(".dat") and not file.endswith("r.dat") and i < 10:
+                i += 1
+                file_paths.append(os.path.join(root, file))
 
-            # Only process .dat files
-            if file.endswith(".dat") and not file.endswith("r.dat"):
+    with Pool(cpu_count()) as p:
+        recordings = p.map(process_file, file_paths)
 
-                file_path = os.path.join(root, file)
+    flat_records = [item for sublist in recordings if sublist for item in sublist]
+    df = pandas.DataFrame(flat_records)
+    df.to_csv("ecg_data.csv", index=False)
+    stopwatch = time.time()- start_time
+    print("TOTAL EXECUTION TIME: ",stopwatch)
 
-                try:
-                    print(f"\nProcessing file: {file}")
+if __name__ == '__main__':
+    from multiprocessing import freeze_support
+    freeze_support()  # optional but recommended on Windows
 
-                    # ---- Step 1: Load ECG ----
-                    signal, fs = load_dat_ecg(file_path)
-                    print(f"Sampling rate (fs): {fs}")
+    # Root directory containing WFDB records
+    data_root_dir = "C:/Users/ZelenePC/Desktop/sleep_apnea_detection/apnea-ecg-database-1.0.0"
 
-                    # ---- Step 2: Preprocess ----
-                    processed_signal = preprocess_signal(signal, fs)
+    # Run processing
+    process_all_ecg_files(data_root_dir)
 
-
-                    print("Processed signal and new fs done: ", processed_signal)
-                    #select one channel from filtered signal
-                    lead_signal = processed_signal[0]
-                    print("lead signal: ", lead_signal)
-
-                    #PROBLEM OCCURED SOMEWHERE HERE while downsampling ran right after filtering
-                    # decided to downsample AFTER RR peak detection
-                    # Detect QRS
-                    qrs_indices = detect_qrs_complex(lead_signal, fs)
-                    print("qrs done")
-                    # Compute RR intervals
-                    rr_intervals = calculate_rr_intervals(qrs_indices, fs)
-                    print("rr_intervals done")
-
-                    #downsample after qrs and select the same channel as lead
-                    downsampled_signal, new_fs = downsampling(processed_signal, fs)
-                    lead_signal_downsampled = downsampled_signal[0]
-                    print("lead signal: ", lead_signal_downsampled)
-
-                    # Plot
-                    segment = lead_signal_downsampled[:5000]  # first 50 seconds
-                    plot_ecg(segment, new_fs, title=file)
-
-                    print(f"Number of QRS complexes: {len(qrs_indices)}")
-                    print(f"Average RR interval: {np.mean(rr_intervals):.3f} s")
-                    print(f"Minimum RR interval: {np.min(rr_intervals):.3f} s")
-                    print(f"Maximum RR interval: {np.max(rr_intervals):.3f} s")
-                    print("--------------------------------------------------")
-
-
-                except Exception as e:
-                    print(f"Error processing file {file}: {e}")
-
-        stopwatch = start_time - datetime.datetime.now()
-        print("TOTAL EXECUTION TIME: ",stopwatch)
-
-
-# ================================
-# ENTRY POINT
-# ================================
-
-# Root directory containing WFDB records
-data_root_dir = "C:/Users/ZelenePC/Desktop/sleep_apnea_detection/apnea-ecg-database-1.0.0"
-
-# Run processing
-process_all_ecg_files(data_root_dir)
