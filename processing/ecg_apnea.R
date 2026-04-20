da6c5d2314d9581d1140fe005e708b5dcf70f5ba
alexandra-balarova <24IMC10507@fh-krems.ac.at>
2026-04-20 21:50:20 +0200

    updated R file lol

--- processing/ecg_apnea.R
+++ processing/ecg_apnea.R
@@ -4,7 +4,8 @@ library(plotly)
 dt <- fread("ecg_data.csv")
 
 colSums(is.na(dt))
-
+dt <- dt[!is.na(dt$label), ]
+#remove signal that isnt labelled
 dt[rowSums(is.na(dt)) > 0, patient]
 #10 patients with missing data
 na_patients <- dt[!complete.cases(dt), patient]
@@ -16,13 +17,11 @@ Q1 <- quantile(dt$rr_mean, .25)
 Q3 <- quantile(dt$rr_mean, .75)
 IQR <- IQR(dt$rr_mean)
 outliers <- subset(dt, dt$rr_mean<(Q1 - 1.5*IQR) | dt$rr_mean>(Q3 + 1.5*IQR))
-dt <- dt[patient != "a04"]
-#a04 had bpm= 11
 
 dt[, .(.N), patient]
 
 
-a <- plot_ly(data = dt[patient == "a01"], x = 1:nrow(dt[patient == "a01"]), y = ~rr_mean, color = ~label,
+a <- plot_ly(data = dt[patient == "a04"], x = 1:nrow(dt[patient == "a04"]), y = ~rr_mean, color = ~label,
              type = "scatter", mode = "markers", showlegend = T)
 a
 
